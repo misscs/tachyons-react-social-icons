815fc8c84d14271f9ff3aff6c7ef90ed273c5de8
claudina sarahe <cs@misscs.me>
2017-12-18 13:54:51 -0500

    Update script

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,7 +6,7 @@ BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 
 echo "Creating v$PACKAGE_VERSION release in $RELEASE_DIR, current branch is '$BRANCH_NAME'"
 
-npm run prepublish
+npm run prepare
 git branch -D build
 git checkout --orphan build
 find . -mindepth 1 -maxdepth 1 ! -name ".git" ! -name ".gitignore" ! -name $RELEASE_DIR ! -name "README.md" ! -name "LICENSE.md" ! -name "node_modules" ! -name "package.json" -exec rm -r "{}" \;
@@ -17,4 +17,4 @@ git commit -m "Release v$PACKAGE_VERSION"
 git tag -a v$PACKAGE_VERSION -m "Release v$PACKAGE_VERSION"
 git push origin v$PACKAGE_VERSION
 git checkout $BRANCH_NAME
-npm run prepublish
+npm run prepare
